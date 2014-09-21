89d8242079ad8074a9ae112eb12a2bcd060bbeac
eric <ealang@gmail.com>
2014-09-21 17:54:47 -0500

    Remote test code

--- mininote/mininote.py
+++ mininote/mininote.py
@@ -15,10 +15,3 @@ class Mininote:
         note.content += '<en-note>' + text + '</en-note>'
         note.tagNames = tag_list
         self.note_store.createNote(note)
-
-if __name__ == '__main__':
-    dev_token = "S=s1:U=8f3c2:E=14f3d61c864:C=147e5b099f0:P=1cd:A=en-devtoken:V=2:H=86cf060ac57d976f657e23eb6f587c3f"
-    mn = Mininote(dev_token)
-
-    note = "this is a #test #note"
-    mn.add_note(note, ['test', 'note'])
