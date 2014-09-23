62ea10d33eeaf4b51558ed3262db4bdb841a07ac
may <zhaomei36@yahoo.com>
2014-09-22 23:44:59 -0500

    Added get_tags() and modified main for some testing.

--- mininote/mininote.py
+++ mininote/mininote.py
@@ -1,5 +1,6 @@
 from evernote.api.client import EvernoteClient
 from evernote.edam.type.ttypes import Note
+import re
 
 
 class Mininote:
@@ -15,3 +16,19 @@ class Mininote:
         note.content += '<en-note>' + text + '</en-note>'
         note.tagNames = tag_list
         self.note_store.createNote(note)
+
+
+def get_tags(note):
+    return re.findall(r"#(\w+)", note)
+
+
+
+if __name__ == '__main__':
+    dev_token = "your dev token here"
+    mn = Mininote(dev_token)
+
+    note = "this is a #test #note"
+    mn.add_note(note, get_tags(note))
+
+    print get_tags(note)
+    print get_tags('test tags #mini #Mini')
